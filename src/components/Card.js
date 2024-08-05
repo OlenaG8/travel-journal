ea00b2ef2f083c6da19be36ae58a7a8466f48752
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-08-05 23:01:12 +0200

    Add Card component for travel destinations

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function Card(props) {
+    console.log(props)
+    return (
+        <div className="card">
+            <img 
+                src={`../images/${props.coverImg}`} 
+                className="card--image" 
+            />
+            <div className="card--location">
+                <img src="../images/location-dot.png" className="card--icon" />
+                <h4>{props.location}</h4>
+            </div>
+            <h5 className="card--title">{props.title}</h5>
+            <a href={props.googleMaps}>View on Google Maps</a>
+            <h6 className="card--price">{props.description}</h6>
+        </div>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        id: 1,
+        title: "Caminito del Rey",
+        location: "Malaga, Spain",
+        description: `This almost 8-kilometre path near Malaga was once considered one of the most dangerous in the world. 
+            Today the entire route has been carefully restored, but although it's now much safer, one thing hasn't changed 
+            - the unique experience of strolling along walkways hanging over 100 metres up on a sheer cliff face. 
+            The views are unforgettable. `,
+        coverImg: "caminito-del-rey.jpg",
+        googleMaps: "https://maps.app.goo.gl/wYoARo44kw3Waztj7",
+    }
+]
