3097b90dec230f1d206f966f4d68f820d2f47fa2
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-08-05 21:26:32 +0200

    Add Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="../images/hero-section.png" className="hero--photo"/>
+            <div className="hero--text">
+                <h2 className="hero--header">Travel Journal</h2>
+                <h3 className="hero--text">Discover breathtaking destinations and unforgettable adventures 
+                    with our mesmerizing travel journal, your ultimate guide to exploring the world.
+                </h3>
+            </div>
+        </section>
+    )
+}
